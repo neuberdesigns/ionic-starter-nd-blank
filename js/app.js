76afb9c4ac319379930584581ea8abfbc7ddd6e5
Neuber Oliveira <neuberdesigns@hotmail.com>
2016-07-19 20:40:12 -0300

    improvments ;)

--- js/app.js
+++ js/app.js
@@ -5,8 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
+angular
+	.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+	.module('starter.controllers', []);
+	.module('starter.services', [])
+	
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
 	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -34,7 +37,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 	// setup an abstract state for the tabs directive
 	.state('index', {
 		url: '/index',
-		templateUrl: 'templates/index.html'
+		templateUrl: 'templates/index.html',
+		controller: 'IndexController'
 	});
 
 	// if none of the above states are matched, use this as the fallback

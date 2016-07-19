a9c1ecb70bf42beb014f2b2af9d6d12eb1f2cced
Neuber Oliveira <neuberdesigns@hotmail.com>
2016-07-19 20:50:35 -0300

    adjust in the call for module in app.js

--- js/app.js
+++ js/app.js
@@ -5,12 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular
-	.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-	.module('starter.controllers', []);
-	.module('starter.services', [])
-	
-.run(function($ionicPlatform) {
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services']);
+angular.module('starter.controllers', []);
+angular.module('starter.services', []);
+
+angular.module('starter').run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
 	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 	// for form inputs)

f5c330eeee4e4f69af507e40166b7d7978df7c33
Neuber Oliveira <neuber.oliveira05@gmail.com>
2016-12-15 14:13:55 -0200

    add - MenuService with the services for  menu, share and rate

--- js/app.js
+++ js/app.js
@@ -13,18 +13,18 @@ angular.module('starter.directives', []);
 
 angular.module('starter').run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
-	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-	// for form inputs)
-	if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-		cordova.plugins.Keyboard.disableScroll(true);
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+			cordova.plugins.Keyboard.disableScroll(true);
 
-	}
-	if (window.StatusBar) {
-		// org.apache.cordova.statusbar required
-		StatusBar.styleDefault();
-	}
-});
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
